59c3a8ced47e6307c5a305ba017bb7e997aeb203
lllMISHRAlll <soumyaranjanmishra29@gmail.com>
2025-05-15 16:03:05 +0530

    CORS Origins set and modified

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ import authRoutes from "./routes/auth.js";
 import searchRoutes from "./routes/searchHistory.js";
 import { errorHandler } from "./middlewares/errors.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+dotenv.config();
+
+const allowedOrigins = [process.env.FRONTEND_URL];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error("CORS blocked:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/search", searchRoutes);
